983c4607988206f628855d23aa78115dd825c0e4
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2024-12-08 16:54:54 +0800

    exercising props
    Pass a prop with all the data and with intended data only

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,14 +1,21 @@
 <script>
 import counter from './components/counter.vue';
+import userDetails from './components/user-details.vue';
 
 export default {
   components : {
-    counter,
+    counter, userDetails,
   },
   data() {
     return {
       message: 'Hello vue!',
       listofNumbers: [1,2,3,4,5],
+      userDetails : {
+        name : 'Hafiz',
+        title : 'Software Engineer',
+        favouriteFramework : 'VueJs',
+        favouriteFood : 'Fried Chicken',
+      }
     }
   },
 };
@@ -16,6 +23,8 @@ export default {
 </script>
 
 <template>
+  <!-- <userDetails :user="userDetails" /> -->
+   <userDetails />
   <counter />
   <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
   <p v-else>Odd: {{ message }}</p>
---
+++ sandbox/src/components/user-details.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    props : {
+        // user : {
+        //     type : Object,
+        //     required : true,
+        // }
+        name : {
+            type : String,
+            required : true,
+            default : 'Jon Doe',
+        },
+        food : {
+            type : String,
+            required : true,
+            default : 'Pizza',
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- <h1>Name : {{ user.userName }}</h1>
+    <h1>Favourite Food : {{ user.favouriteFood }}</h1> -->
+
+    <h2>Name : {{ name }}</h2>
+    <h2>Food : {{ food }}</h2>
+
+</template>
