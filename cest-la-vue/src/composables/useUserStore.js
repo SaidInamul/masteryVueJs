f9fd5a3eef0b7e1671f539d41529d5e5a657f1f9
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2024-12-12 00:30:33 +0800

    Delete cest-la-vue directory

--- cest-la-vue/src/composables/useUserStore.js
+++
@@ -1,6 +0,0 @@
-// import { ref } from "vue";
-
-// const response = await fetch("https://jsonplaceholder.typicode.com/users")
-// const data = await response.json();
-
-// export const users = ref(data);
